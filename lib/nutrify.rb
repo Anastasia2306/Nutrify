83525e1611a92ba717e992132931895a61c01576
Polina <dyatlowskaya.polina@yandex.ru>
2026-03-26 02:12:25 +0300

    fix: адаптер Additive для интеграции с NutriAnalyzer

--- lib/nutrify.rb
+++ lib/nutrify.rb
@@ -4,17 +4,31 @@ require_relative "nutrify/version"
 require_relative "nutrify/db_manager"
 require_relative "nutrify/product"
 require_relative "nutrify/client"
+require "ostruct"
 
 module Nutrify
   class Error < StandardError; end
 
   class Additive
     def self.find_by_code(code)
-      data = Nutrify::DbManager.find(code)
+      clean_code = code.to_s.downcase
+      data = Nutrify::DbManager.find(clean_code) || Nutrify::DbManager.find("en:#{clean_code}")
+
       return nil unless data
 
-      require "ostruct"
-      OpenStruct.new(data)
+      obj = OpenStruct.new(data)
+
+      obj.code ||= code.to_s.upcase
+      obj.allergens ||= []
+      obj.category ||= "Пищевая добавка"
+
+      obj.daily_limit_mg_per_kg ||= if obj.code.include?("621")
+                                      30
+                                    else
+                                      0
+                                    end
+
+      obj
     end
   end
 end
