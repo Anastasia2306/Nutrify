dd96b012f2cc01c59fd1166995a750974cf1062f
Polina <dyatlowskaya.polina@yandex.ru>
2026-03-26 15:37:51 +0300

    fix: исправление ошибок (rubocop)

--- lib/nutrify.rb
+++ lib/nutrify.rb
@@ -4,22 +4,27 @@ require_relative "nutrify/version"
 require_relative "nutrify/db_manager"
 require_relative "nutrify/product"
 require_relative "nutrify/client"
-require "ostruct"
 
 module Nutrify
   class Error < StandardError; end
 
+  # Адаптер для добавок
   class Additive
+    Data = Struct.new(:code, :name, :allergens, :contraindications, :risks,
+                      :category, :daily_limit_mg_per_kg, keyword_init: true)
+
     def self.find_by_code(code)
       raw_data = find_raw_data(code)
       return nil unless raw_data
 
-      enrich_data(OpenStruct.new(raw_data), code)
+      # Безопасно превращаем ключи в символы для Struct
+      data_hash = raw_data.each_with_object({}) { |(k, v), h| h[k.to_sym] = v }
+      obj = Data.new(**data_hash)
+      enrich_data(obj, code)
     end
 
     def self.find_raw_data(code)
       clean = code.to_s.downcase
-
       Nutrify::DbManager.find(clean) ||
         Nutrify::DbManager.find("en:#{clean}") ||
         Nutrify::DbManager.find(code.to_s.upcase)
@@ -27,23 +32,27 @@ module Nutrify
 
     def self.enrich_data(obj, original_code)
       obj.code ||= original_code.to_s.upcase
-
-      obj.name ||= if obj.code.include?("322")
-                     "Лецитин"
-                   else
-                     (obj.code.include?("621") ? "Глутамат натрия" : "Добавка")
-                   end
-
-
-      obj.allergens ||= []
-      obj.contraindications ||= []
-      obj.risks ||= []
-
+      obj.name ||= detect_name(obj.code)
       obj.category ||= "Пищевая добавка"
       obj.daily_limit_mg_per_kg ||= (obj.code.include?("621") ? 30 : 0)
+
+      fill_arrays(obj)
       obj
     end
 
-    private_class_method :find_raw_data, :enrich_data
+    def self.fill_arrays(obj)
+      obj.allergens ||= []
+      obj.contraindications ||= []
+      obj.risks ||= []
+    end
+
+    def self.detect_name(code)
+      return "Лецитин" if code.include?("322")
+      return "Глутамат натрия" if code.include?("621")
+
+      "Добавка"
+    end
+
+    private_class_method :find_raw_data, :enrich_data, :fill_arrays, :detect_name
   end
 end
